d3e73656a6b3452a7d251a9d32fcd968e6239f2a
Paradox04 <35827226+Paradox04@users.noreply.github.com>
2018-01-26 14:54:53 -0700

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,18 @@
+//hello
+var x = 2;
+var work = ["Does this work?",true,false,3,8,"Nathaniel"];
+function hello(x,y) {
+	// body...
+	console.log('HAPPY BIRTHDAY')
+}
+hello(5,9);
+console.log("HI");
+alert('SOMETHING NEEDS TO HAPPEN!!!');
+if(work[1]===true){
+	alert(work[0]);
+}else{
+	work[1] = true;
+}
+if(work[2]===false){
+	work[1] = false;
+}
